e36e415f8141803904a0dd2b22a62816822dd1b8
ShauryaNarayan <shauryan240701@gmail.com>
2026-01-17 11:22:26 +0530

    Changed the baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://store-rating-managment-application-4.onrender.com/api',
 });
 
 // Automatically add the Token to every request if we have one
